77db3bc3343e78a48beea43a78ccc8231a65977b
Naser Abdulkadir <naserabdulkadir0@gmail.com>
2021-10-18 20:01:24 -0400

    got the hamburger menu working but i have a good feeling that i can refactor the code a little more in the js and css.

--- main.js
+++ main.js
@@ -2,8 +2,22 @@
 
 const hamburgerIcon = document.querySelector(".hamburger-icon");
 const activeNav = document.querySelector(".active-navbar");
+const navbar = document.querySelector(".navbar");
+const header = document.querySelector(".name");
 
-hamburgerIcon.addEventListener("click", function () {
-  activeNav.classList.toggle("hidden");
-  hamburgerIcon.src = "/Room-Homepage/images/icon-close.svg";
-});
+hamburgerIcon.addEventListener("click", toggleState);
+
+function toggleState() {
+  if (activeNav.classList.contains("hidden")) {
+    console.log("kdfhnag");
+    activeNav.classList.toggle("hidden");
+    hamburgerIcon.src = "/Room-Homepage/images/icon-close.svg";
+    navbar.style.backgroundColor = "white";
+    header.style.display = "none";
+  } else {
+    activeNav.classList.toggle("hidden");
+    hamburgerIcon.src = "/Room-Homepage/images/icon-hamburger.svg";
+    navbar.style.removeProperty("background-color");
+    header.style.display = "flex";
+  }
+}
