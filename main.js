589f64052376bbffbc221a0c019bc41b430fda54
Naser Abdulkadir <naserabdulkadir0@gmail.com>
2021-10-22 19:18:39 -0400

    started the project from scratch and got back to where i was before on both screens. i had to restart because i was only thinking about one device when making the layout orgininally and it messed up the desktop layout. i recreated it with both in mind and it looks better now and runs better than before. im gonna continue to build it like this but the next set of buttons is gonna be a little tricky to implement.

--- main.js
+++ main.js
@@ -1,23 +1,26 @@
-const hamburgerButton = document.querySelector(".hamburger-icon");
-const navbar = document.querySelector(".navbar");
-const header = document.querySelector(".header");
-const headerArea = document.querySelector(".header-area");
+"use strict";
+const hamburgerButton = document.querySelector(".hamburger-button");
+const hamburgerIcon = document.querySelector(".hamburger-icon");
+const header = document.querySelector(".top-picture-header");
+const hamburgerNavbar = document.querySelector(".hamburger-navbar");
+const navLinks = document.querySelector(".nav-links-mobile");
+let isOpen = false;
 
-// hamburger menu functionality //
-hamburgerButton.addEventListener("click", press);
+// hamburger menu functionallity //
+hamburgerButton.addEventListener("click", pressed);
 
-function press() {
-  if (navbar.classList.contains("hidden")) {
-    navbar.classList.toggle("hidden");
-    headerArea.style.backgroundColor = "white";
+function pressed() {
+  if (isOpen === false) {
+    hamburgerIcon.src = "/Room-Homepage/images/icon-close.svg";
     header.style.display = "none";
-    navbar.style.display = "flex";
-    hamburgerButton.src = "/Room-Homepage/images/icon-close.svg";
-  } else {
-    navbar.classList.toggle("hidden");
-    headerArea.style.removeProperty("background-color");
-    navbar.style.display = "none";
+    navLinks.style.display = "flex";
+    hamburgerNavbar.style.backgroundColor = "white";
+    isOpen = true;
+  } else if (isOpen === true) {
+    hamburgerIcon.src = "/Room-Homepage/images/icon-hamburger.svg";
     header.style.display = "flex";
-    hamburgerButton.src = "/Room-Homepage/images/icon-hamburger.svg";
+    navLinks.style.display = "none";
+    hamburgerNavbar.style.removeProperty("background-color");
+    isOpen = false;
   }
 }
