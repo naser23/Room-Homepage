5358d1bd317796780abf5ac725b0659213edfbc2
Naser Abdulkadir <naserabdulkadir0@gmail.com>
2021-11-02 16:01:05 -0400

    added some variables im gonna need to get the image sliders working and changed some of the text sizes for the larger screens.

--- main.js
+++ main.js
@@ -10,6 +10,19 @@ const rightSliderMobile = document.querySelector(".right-slider-mobile");
 const topArea = document.querySelector("#top-area-with-navbar");
 let isOpen = false;
 
+// varriables needed for slider //
+let header1 = "Discover innovative ways to decorate";
+let mainText1 =
+  "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make you property a refelction of you and what you love.";
+
+let header2 = "We are available all across the globe";
+let mainText2 =
+  "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.";
+
+let header3 = "Manufactured with the best materials";
+let mainText3 =
+  "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.";
+
 // hamburger menu functionallity //
 hamburgerButton.addEventListener("click", pressed);
 
@@ -36,3 +49,4 @@ leftSliderMobile.addEventListener("click", function () {
 rightSliderMobile.addEventListener("click", function () {
   console.log("right arrow pressed");
 });
+``;
